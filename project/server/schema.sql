94bd1d9cae0252f2ed9210f3b46df383ebd26e40
Mohamedaziz1919 <guerfalamohamed2001@gmail.com>
2023-05-18 19:15:47 +0200

    .

--- project/server/schema.sql
+++ project/server/schema.sql
@@ -14,15 +14,14 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema zara
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS zara DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE zara ;
-
+CREATE SCHEMA IF NOT EXISTS `zara` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `zara` ;
 
 
 
 
 -- -----------------------------------------------------
--- Table zara.`users`
+-- Table `zara`.`products`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `zara`.`products` (
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -34,14 +33,6 @@ CREATE TABLE IF NOT EXISTS `zara`.`products` (
   `image` VARCHAR(500) NOT NULL,
   `description` VARCHAR(500) CHARACTER SET 'armscii8' NOT NULL,
   PRIMARY KEY (`id`))
-CREATE TABLE IF NOT EXISTS zara.`users` (
-  id INT NOT NULL AUTO_INCREMENT,
-  firstname VARCHAR(45) NOT NULL,
-  lastname VARCHAR(45) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE INDEX email_UNIQUE (email ASC) VISIBLE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
@@ -80,26 +71,16 @@ CREATE TABLE IF NOT EXISTS `zara`.`cart` (
   CONSTRAINT `fk_users_has_products_products1`
     FOREIGN KEY (`products_id`)
     REFERENCES `zara`.`products` (`id`)
-CREATE TABLE IF NOT EXISTS zara.`cart` (
-  users_id INT NOT NULL,
-  products_id INT NOT NULL,
-  PRIMARY KEY (users_id, products_id),
-  INDEX fk_users_has_products_products1_idx (products_id ASC) VISIBLE,
-  INDEX fk_users_has_products_users_idx (users_id ASC) VISIBLE,
-  CONSTRAINT fk_users_has_products_users
-    FOREIGN KEY (users_id)
-    REFERENCES zara.`users` (id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_users_has_products_products1
-    FOREIGN KEY (products_id)
-    REFERENCES zara.`products` (id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
