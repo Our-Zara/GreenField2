4e7aa56c3d416979cc163191109b36af7591e067
malaksboui <sboui.95malak@yahoo.com>
2023-05-21 21:25:41 +0100

    done

--- project/pages/[gender]/[category].tsx
+++ project/pages/[gender]/[category].tsx
@@ -11,6 +11,7 @@ import {
 } from "mdb-react-ui-kit";
 
 interface Product {
+  id: number;
   name: string;
   image: string;
   price: number;
@@ -20,11 +21,10 @@ interface Product {
   description: string;
 }
 
-
 const ProductPage = () => {
   const [gender, setGender] = useState("");
   const [category, setCategory] = useState("");
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<Product[]>([]);
   const [cart, setCart] = useState<Product[]>([]);
 
   const fetchData = () => {
@@ -32,12 +32,10 @@ const ProductPage = () => {
       .get(`http://localhost:4001/zara/product/${gender}/${category}`)
       .then((res) => {
         setData(res.data);
-        
       })
       .catch((err) => {
         console.log(err);
       });
-      
   };
 
   useEffect(() => {
@@ -49,26 +47,41 @@ const ProductPage = () => {
   }, [category, gender]);
 
   const addToCart = (product: Product) => {
-    setCart([...cart, product]);
+    const Newcart = {
+      users_id: 1, 
+      products_id: product.id,
+    };
+
+    axios
+      .post("http://localhost:4001/zara/cart/add", Newcart)
+      .then((res) => {
+        console.log("Product added to cart:", res.data);
+        setCart([...cart, product]);
+      })
+      .catch((err) => {
+        console.error("Failed to add product to cart:", err);
+      });
   };
 
   return (
     <MDBContainer style={{ marginTop: "150px" }}>
       <MDBRow className="justify-content-center">
-        {data.map((e:any) => (
-          <MDBCol md="12" lg="4" className="mb-4" key={e.id}>
+        {data.map((product) => (
+          <MDBCol md="12" lg="4" className="mb-4" key={product.id}>
             <MDBCard className="h-100">
-              <MDBCardImage src={e.image} alt={e.name} className="w-100" />
+              <MDBCardImage src={product.image} alt={product.name} className="w-100" />
               <MDBCardBody className="d-flex flex-row justify-content-between p-0 pt-1.5">
                 <span className="card-title mb-3 text-left details" style={{ fontFamily: "Arial, Helvetica, sans-serif", fontSize: "15px", paddingLeft:"20px", paddingTop :"5px" }}>
-                  {e.name}
+                  {product.name}
                 </span>
-                <span className="mb-3 text-right details" style={{ fontFamily: "Arial, Helvetica, sans-serif", fontSize: "15px", paddingRight:"20px", paddingTop :"5px" }}>${e.price}</span>
+                <span className="mb-3 text-right details" style={{ fontFamily: "Arial, Helvetica, sans-serif", fontSize: "15px", paddingRight:"20px", paddingTop :"5px" }}>
+                  ${product.price}
+                </span>
               </MDBCardBody>
               <MDBBtn
                 style={{ backgroundColor: "white", color: "black" }}
                 className="btn-white-hover"
-                onClick={() => addToCart(e)}
+                onClick={() => addToCart(product)}
               >
                 Add to Cart
               </MDBBtn>
